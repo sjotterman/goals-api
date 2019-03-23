ff407b896e46475829a95f78fe694c096063a9ac
Samuel Otterman <sjotterman@gmail.com>
2019-03-23 17:24:08 -0400

    install and set up nodemon

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello world.');
+app.get("/", (req, res) => {
+  res.send("Hello world!");
 });
 
 app.listen(port, () => {
