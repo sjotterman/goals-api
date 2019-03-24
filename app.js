82b37b26720b8b6636c61bab379bea24c0847ac6
Samuel Otterman <sjotterman@gmail.com>
2019-03-24 19:20:09 -0400

    post goal

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const bodyParser = require("body-parser");
 const app = express();
 const dbConnString =
   process.env.DB_CONN_STRING || "mongodb://localhost/goalsAPI-dev";
@@ -10,19 +10,29 @@ const goalRouter = express.Router();
 const port = process.env.PORT || 4000;
 const Goal = require("./models/goalModel");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(cors());
-goalRouter.route("/goals").get((req, res) => {
-  const query = {};
-  if (req.query.type) {
-    query.type = req.query.type;
-  }
-  Goal.find(query, (err, goals) => {
-    if (err) {
-      return res.send(err);
+goalRouter
+  .route("/goals")
+  .post((req, res) => {
+    const goal = new Goal(req.body);
+    goal.save();
+    return res.status(201).json(goal);
+  })
+  .get((req, res) => {
+    const query = {};
+    if (req.query.type) {
+      query.type = req.query.type;
     }
-    return res.json(goals);
+    Goal.find(query, (err, goals) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(goals);
+    });
   });
-});
 goalRouter.route("/goals/:goalId").get((req, res) => {
   Goal.findById(req.params.goalId, (err, goal) => {
     if (err) {
