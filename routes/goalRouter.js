4eb271642586c8725d9a30d40fc24f8c40094de6
Samuel Otterman <sjotterman@gmail.com>
2019-03-24 21:28:53 -0400

    POST and PATCH

--- routes/goalRouter.js
+++ routes/goalRouter.js
@@ -21,14 +21,53 @@ function routes(Goal) {
         return res.json(goals);
       });
     });
-  goalRouter.route("/goals/:goalId").get((req, res) => {
+  goalRouter.use("/goals/:goalId", (req, res, next) => {
     Goal.findById(req.params.goalId, (err, goal) => {
       if (err) {
         return res.send(err);
       }
-      return res.json(goal);
+      if (goal) {
+        req.goal = goal;
+        return next();
+      }
+      return res.sendStatus(404);
     });
   });
+  goalRouter
+    .route("/goals/:goalId")
+    .get((req, res) => {
+      res.json(req.goal);
+    })
+    .put((req, res) => {
+      const { goal } = req;
+      goal.name = req.body.name;
+      goal.dates_done = req.body.dates_done;
+      goal.type = req.body.type;
+      goal.user_id = req.body.user_id;
+      req.goal.save(err => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(goal);
+      });
+    })
+    .patch((req, res) => {
+      const { goal } = req;
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach(item => {
+        const key = item[0];
+        const value = item[1];
+        goal[key] = value;
+      });
+      req.goal.save(err => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(goal);
+      });
+    });
 
   return goalRouter;
 }
